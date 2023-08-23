0e2f7e4d6f846a66dd4db78b7b48e900c7876fc6
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-23 08:58:46 -0500

    Update Streamlit_massspec_heatmapv2.py

--- Streamlit_massspec_heatmapv2.py
+++ Streamlit_massspec_heatmapv2.py
@@ -80,5 +80,10 @@ p1 = sns.heatmap(dfheatmap_offset, fmt='.0f', annot=dfheatmap_filled, annot_kws=
 ax.set_yticklabels(ax.get_yticklabels(), size=fontsize_tick, weight='bold') ## version 2 addition 
 ## Saving the final heatmap 
 plt.savefig('heatmap.png')
-st.pyplot(clear_figure=False)
+
+if uploaded_file is not None:
+   st.pyplot(fig)
+else:
+   st.stop()
+ 
 save=st.download_button('PNG file name to save', data=open('heatmap.png','rb').read(), file_name='heatmap.png')
