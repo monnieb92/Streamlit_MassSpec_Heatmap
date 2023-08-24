a5e85d0e226bbdc24c3c70b1c0b4f0364232a682
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-24 16:47:41 -0500

    Update Streamlit_massspec_heatmapv2.py

--- Streamlit_massspec_heatmapv2.py
+++ Streamlit_massspec_heatmapv2.py
@@ -16,7 +16,7 @@ from matplotlib.colors import LogNorm
 uploaded_file = st.sidebar.file_uploader("Upload your input csv file", type=["csv"])
 st.write('This is the path to the csv file from Spectral Counts',uploaded_file)
 image = Image.open('HeaderExample.png')
-st.image(caption="Example of the Header:",image)
+st.image(caption="Example of the Header:", image)
  
 fontsize = st.number_input('Font size of spectral counts',value = 8)
 st.write('Font size for the annotations aka spectral counts, default 8', fontsize)
