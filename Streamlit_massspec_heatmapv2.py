2bb0dd110140d23052609b9d2efbf582abfffa57
Monica Bomber <60197768+monnieb92@users.noreply.github.com>
2023-08-24 16:51:10 -0500

    Update Streamlit_massspec_heatmapv2.py

--- Streamlit_massspec_heatmapv2.py
+++ Streamlit_massspec_heatmapv2.py
@@ -45,43 +45,44 @@ st.write(location)
 
 annotation_txt_color=st.color_picker('Color of the spectral counts', value="#fdfdfd")
 st.write('The current color for the spectral counts is', annotation_txt_color)
-if st.button('Make Heatmap'):
-# Read CSV file
-df = pd.read_csv(uploaded_file)
-print("Top 10 rows of the csv file")
-print(df.head(10))
 
-# Extract GeneName
-df['GeneName'] = df.iloc[:,3].str.extract(r'GN=(\w+)')
+if st.button('Make Heatmap'):
+ # Read CSV file
+ df = pd.read_csv(uploaded_file)
+ print("Top 10 rows of the csv file")
+ print(df.head(10))
+ # Extract GeneName
+ df['GeneName'] = df.iloc[:,3].str.extract(r'GN=(\w+)')
 
 # Prepare heatmap data
-dfheatmapv1 = df['GeneName'].to_frame(name='GeneName')
-loc_start, loc_end = map(int, location.split(':'))
-dfheatmap = df.iloc[:, loc_start:loc_end]  # Select columns 9 to 14
-dfheatmap = pd.concat([dfheatmapv1,dfheatmap], axis = 1)
-print("Top 5 rows of the new concatenated data frame for the heatmap")
-print(dfheatmap.head(5))
-dfheatmap = dfheatmap.iloc[0:rows]
+ dfheatmapv1 = df['GeneName'].to_frame(name='GeneName')
+ loc_start, loc_end = map(int, location.split(':'))
+ dfheatmap = df.iloc[:, loc_start:loc_end]  # Select columns 9 to 14
+ dfheatmap = pd.concat([dfheatmapv1,dfheatmap], axis = 1)
+ print("Top 5 rows of the new concatenated data frame for the heatmap")
+ print(dfheatmap.head(5))
+ dfheatmap = dfheatmap.iloc[0:rows]
 ## Set the GeneName column as the row names for each spectral count 
-dfheatmap.set_index('GeneName', inplace=True)
-print("Top 5 rows of the new df for heatmap with the rows labeled as GeneNames")
-print(dfheatmap.head(5))
+ dfheatmap.set_index('GeneName', inplace=True)
+ print("Top 5 rows of the new df for heatmap with the rows labeled as GeneNames")
+ print(dfheatmap.head(5))
 ## Making any NAs into 0s 
-dfheatmap_filled = dfheatmap.fillna(0)
+ dfheatmap_filled = dfheatmap.fillna(0)
 
 # Plot heatmap
-offset = 1e-1
+ offset = 1e-1
 ## Offsetting the heatmap by 0.01 to help adjust the coloring in the heatmap because the logNorm of 0 is -inf
-dfheatmap_offset = dfheatmap_filled + offset
+ dfheatmap_offset = dfheatmap_filled + offset
 ## figure heatmap size 
-fig, ax = plt.subplots(figsize=[width,height])
+ fig, ax = plt.subplots(figsize=[width,height])
 ## plotting heatmap with the offset df as the coloring because the logNorm of 0 is -inf, the annotated spectral counts as dfheatmap_filled (the actual # of spectral counts) and no decimal point fmt='.0f'; cmap is the coloring map/palette you chose or the default; linecolor is always white with a width of 0.5 between each heatmap square; performing the log normalization of the spectral counts for proper coloring  
-p1 = sns.heatmap(dfheatmap_offset, fmt='.0f', annot=dfheatmap_filled, annot_kws={"size": fontsize, "weight": "bold","color":annotation_txt_color}, cmap=color, linecolor='white', linewidth='0.5', norm=LogNorm())
+ p1 = sns.heatmap(dfheatmap_offset, fmt='.0f', annot=dfheatmap_filled, annot_kws={"size": fontsize, "weight": "bold","color":annotation_txt_color}, cmap=color, linecolor='white', linewidth='0.5', norm=LogNorm())
 ## adjust the size and boldness of the y-axis labeling aka the protein/Gene names 
-ax.set_yticklabels(ax.get_yticklabels(), size=fontsize_tick, weight='bold') ## version 2 addition 
+ ax.set_yticklabels(ax.get_yticklabels(), size=fontsize_tick, weight='bold') ## version 2 addition 
 ## Saving the final heatmap 
-plt.savefig('heatmap.png')
-st.pyplot()
+ plt.savefig('heatmap.png')
+ st.pyplot()
 else
-st.write("No .csv uploaded")
+ st.write("No .csv uploaded")
+
 save=st.download_button('PNG file name to save', data=open('heatmap.png','rb').read(), file_name='heatmap.png')
